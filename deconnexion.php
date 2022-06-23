906c0fc4e6fe66fc6422e6e5fc3cfe268caa1f95
Aymenbs22 <90031228+Aymenbs22@users.noreply.github.com>
2022-06-23 23:44:01 +0100

    Deconnexion Page
    Deconnexion Page

---
+++ deconnexion.php
@@ -0,0 +1,7 @@
+<?php
+session_start(); //start session or resume
+session_unset(); // clear el data eli fel session
+session_destroy(); //ysaker el session
+header('Location:homepage.php');
+exit;
+?>
