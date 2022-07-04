7d55fb44510223a700f8d480888c1552b0eca895
Aymenbs22 <90031228+Aymenbs22@users.noreply.github.com>
2022-07-04 11:42:33 +0100

    Cleanup my Portfolio Project
    Cleanup my Portfolio Project

--- deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); //start session or resume
-session_unset(); // clear el data eli fel session
-session_destroy(); //ysaker el session
-header('Location:homepage.php');
-exit;
-?>
