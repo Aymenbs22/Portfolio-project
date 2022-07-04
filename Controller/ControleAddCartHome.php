3f02584c2a359b1f0290f46e9e81c990f4795366
Aymenbs22 <90031228+Aymenbs22@users.noreply.github.com>
2022-07-04 11:59:41 +0100

    Controle Add Cart Home
    Controle Add Cart Home to add add items in cart

---
+++ Controller/ControleAddCartHome.php
@@ -0,0 +1,30 @@
+<?php
+require '../_header.php';
+$val = $_GET['ref'];
+//  var_dump($val);die;for testing  
+// To Test If We Get The Refrance Or Not 
+
+
+//If We Get It
+if(isset($_GET['ref'])) {
+     // we search on it in data base (we make sure that we will find)
+     //We Can use val or ref
+      $req = $DB->db->prepare('SELECT reference FROM products WHERE reference=:ref', array('ref'=> $_GET['ref']));
+      $req->execute(array('ref'=> $_GET['ref']));
+      $products = $req->fetchAll(); //it give us the reference of selected product
+     /*  var_dump($products);die; */
+    
+
+   if(empty($products)){
+       die("This product is not available");
+   }
+
+   //if we find it we will add on session (call a method from cart.class)
+   //we can call ($Cart) because we cal it in page _header.php($cart= new cart())
+   //if we don't call it in _header's page we will get an error 
+   $cart->add($products[0]['reference']);
+    header('Location: ../pages/acceuil.php');
+    die(" Product added to cart ");
+}else{
+    die("No product selected");
+}
