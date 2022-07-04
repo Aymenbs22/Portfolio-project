76faab25303796ab9cf154287652a8bf0004a774
Aymenbs22 <90031228+Aymenbs22@users.noreply.github.com>
2022-07-04 11:51:21 +0100

    Appointments Class
    Appointments Class to insert appointment into the data base

---
+++ Class/Appointement.class.php
@@ -0,0 +1,40 @@
+<?php
+class Appointement 
+{
+    private $name;
+    private $date;
+    private $time;
+    private $mail; 
+    private $type;
+    public $number;
+
+    //private $quantite
+    public function __construct($name, $date, $time ,$mail,$type,$number)
+    {
+        $this->name = $name;
+        $this->date = $date;
+        $this->time = $time;   
+        $this->mail = $mail; 
+        $this->type = $type;
+        $this->number = $number;
+
+    }
+    public static function AddRdv($rdv)
+    {
+         /* we make include _header to called db.class.php 
+         to define a DB object ( $DB=new DB()) */
+       include("../_header.php");
+        $statement = $DB->db->prepare("INSERT INTO 
+        appointment (nom,dates,exactTime,mail,types,PhoneNumber) VALUES(?,?,?,?,?,?);");
+        $statement->bindParam('1', $rdv->name);
+        $statement->bindParam('2', $rdv->date);
+        $statement->bindParam('3', $rdv->time);
+        $statement->bindParam('4', $rdv->mail); 
+        $statement->bindParam('5', $rdv->type);
+        $statement->bindParam('6', $rdv->number); 
+         
+        $statement->execute() ;
+    }
+}
+
+?>
